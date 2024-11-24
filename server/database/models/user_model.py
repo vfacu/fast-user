846df60a2b435e98e6f09fc6fcfa29e2af48ca34
Facundo <vfacu@hotmail.com>
2024-11-24 20:37:30 -0300

    creo Capa de Modelo

--- server/database/models/user_model.py
+++ server/database/models/user_model.py
@@ -8,10 +8,9 @@ from .base_model import BaseModel
 class UserModel(BaseModel):
     __tablename__ = 'users'
 
-    legajo: Mapped[int] = mapped_column(Integer, nullable=False, unique=True)
-    dni: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
+    legajo: Mapped[int] = mapped_column(Integer, nullable=False)
+    dni: Mapped[str] = mapped_column(String(20), nullable=False)
     nombre: Mapped[str] = mapped_column(String(50), nullable=False)
     apellido: Mapped[str] = mapped_column(String(50), nullable=False)
     telefono: Mapped[str] = mapped_column(String(15), nullable=False)
-    email: Mapped[EmailStr] = mapped_column(
-        String(100), nullable=False)
+    email: Mapped[EmailStr] = mapped_column(String(100), nullable=False)
